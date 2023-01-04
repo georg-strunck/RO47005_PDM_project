53d6cefed7bbeffdae21c3c80430e6be20402f2e
Georg_Strunck <georg.strunck@t-online.de>
2023-01-04 17:08:17 +0100

    Added comments and implemented continous loop for csv_path and export bbox and position to csv

--- my_main.py
+++ my_main.py
@@ -18,8 +18,13 @@ from operator import add    # needed for list addition with map()
 client, sim = initialize_simulation()
 
 # Get building sizes and positions
-num_buil = 6            # number of buildings
+num_buil = 11            # number of buildings
 pos_buil, bbox_bui = get_pos_and_bbox(num_buil, sim)
+
+# Export center positions of buildings
+np.savetxt("center_positions_objects.csv", pos_buil, delimiter=",")
+# Export bounding box sizes of buildings
+np.savetxt("bboxes_objects.csv", bbox_bui, delimiter=",")
 
 # Get handles of quadcopter target (green ball) and of quadcopter itself for later use
 target_handle = sim.getObject('./Quadcopter/target')
@@ -27,25 +32,38 @@ quad_handle = sim.getObject('./Quadcopter')
 # Current (starting) position of quadcopter target
 target_position = get_position(target_handle, sim)
 
+# Load path from csv file
+path_from_csv = np.genfromtxt("final_path.csv", delimiter=",")
 
+# Set both the quadcopter and target to the starting position of the loaded path
+startpoint_list = path_from_csv[0].tolist()
+sim.setObjectPosition(target_handle, sim.handle_world, startpoint_list)
+sim.setObjectPosition(quad_handle, sim.handle_world, startpoint_list)
+
+# Get the starting position of the quadcopter
 start = get_position(quad_handle, sim)
-path = np.array([start,
-                 [-1.75, -3., 0.5], 
-                 [-1.6, -3.7, 3.], 
-                 [-1.6, -6., 3.],
-                 [-1.75, -7., 0.5],
-                 [2.38, -5.7, 0.5],
-                 start])
+
+# Define the maximum stepsize of the target per time step (~ to speed)
 max_stepsize = 0.08
-my_drone_path = generate_drone_trajectory_from_path_array(start, max_stepsize, path)
+# Generate the path with straight segments in between so not to destabilize the drone
+my_drone_path_csv = generate_drone_trajectory_from_path_array(start, max_stepsize, path_from_csv)
 
+# Run until time 40 
 while (t := sim.getSimulationTime()) < 40:
-    for waypoint in my_drone_path:
-        print('Simulation time: ', round(sim.getSimulationTime(), 2), '\t[s]')
+    # Fly through path
+    for waypoint in my_drone_path_csv:
+        # Print current time
+        print('Simulation time: ', round(sim.getSimulationTime(), 2), '  \t[s]')
+        # Convert waypoint to list for coppelia sim
         waypoint_list = waypoint.tolist()
+        # Update the target position to the waypoint (so the drone will fly towards it)
         sim.setObjectPosition(target_handle, sim.handle_world, waypoint_list)
+        # Step the simulation forward (time step)
         client.step()
+    # Reverse path and fly back
+    my_drone_path_csv = my_drone_path_csv[::-1]
 
+# Stop the simulation
 sim.stopSimulation()
 
 
